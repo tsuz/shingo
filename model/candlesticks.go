ed563206b2db72fcd1c4b599f04fb7118f3ddbe6
tsuz <tak215@gmail.com>
2018-04-24 03:46:29 +0900

    aggregating multiple 'candlestick's as candlesticks

---
+++ model/candlesticks.go
@@ -0,0 +1,44 @@
+package model
+
+type CandlesticksMeta interface {
+	Total() int
+	Interval() Interval
+	ItemAtIndex(idx int) *Candlestick
+}
+
+type Candlesticks struct {
+	CandlesticksMeta
+
+	interval Interval
+	items    []*Candlestick
+}
+
+func (cs *Candlesticks) AppendCandlestick(c *Candlestick) error {
+	cs.items = append(cs.items, c)
+	return nil
+}
+
+func NewCandlesticks(i Interval) (*Candlesticks, error) {
+	return &Candlesticks{
+		interval: i,
+		items:    make([]*Candlestick, 0),
+	}, nil
+}
+
+// Total returns total candlesticks
+func (cs *Candlesticks) Total() int {
+	return len(cs.items)
+}
+
+// ItemAtIndex returns the item at specific index
+func (cs *Candlesticks) ItemAtIndex(idx int) *Candlestick {
+	if len(cs.items) > idx {
+		return cs.items[idx]
+	}
+	return nil
+}
+
+// Interval returns currently set interval for the series of candlesticks
+func (cs *Candlesticks) Interval() Interval {
+	return cs.interval
+}
